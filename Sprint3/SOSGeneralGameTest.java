653259b89b0b1335363970f3bf0234a9e2bb9424
A-Alhammadi <113705421+A-Alhammadi@users.noreply.github.com>
2023-10-29 20:50:49 -0500

    Add files via upload

--- Sprint3/SOSGeneralGameTest.java
+++ Sprint3/SOSGeneralGameTest.java
@@ -1,68 +1,61 @@
 package SOSgame;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import SOSgame.SOSGameBase.Cell;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SOSGeneralGameTest {
 
     private SOSGeneralGame game;
 
     @BeforeEach
-    public void setup() {
-        game = new SOSGeneralGame(3); // Assuming a 3x3 grid for simplicity.
+    public void setUp() {
+        game = new SOSGeneralGame(3); // Let's create a 3x3 board for simplicity.
     }
 
     @Test
-    public void testInitialBoardEmpty() {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                assertEquals(Cell.EMPTY, game.getCell(row, col));
+    public void testInitialGameState() {
+        assertFalse(game.isGameOver());
+        assertEquals('S', game.getTurn());
+        assertFalse(game.wasLastMoveSOS());
+    }
+
+    @Test
+    public void testValidMove() {
+        assertTrue(game.makeMove(0, 0, 'S'));
+        assertEquals('O', game.getTurn()); // Turn should change after a valid move.
+        assertEquals(SOSGameBase.Cell.S, game.getCell(0, 0));
+    }
+
+    @Test
+    public void testInvalidMove() {
+        game.makeMove(0, 0, 'S');
+        assertFalse(game.makeMove(0, 0, 'O')); // Attempting to place in an occupied cell should return false.
+    }
+
+    @Test
+    public void testFormingSOS() {
+        game.makeMove(0, 0, 'S');
+        game.makeMove(0, 1, 'O');
+        assertTrue(game.makeMove(0, 2, 'S'));
+        assertTrue(game.wasLastMoveSOS()); // An SOS was formed, so this should be true.
+    }
+
+    @Test
+    public void testNotFormingSOS() {
+        game.makeMove(0, 0, 'S');
+        game.makeMove(0, 1, 'S');
+        game.makeMove(0, 2, 'S');
+        assertFalse(game.wasLastMoveSOS()); // No SOS formed, should be false.
+    }
+
+    @Test
+    public void testGameIsOver() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                game.makeMove(i, j, (i+j) % 2 == 0 ? 'S' : 'O'); // Fill up the board.
             }
         }
-    }
-
-    @Test
-    public void testMakeMove() {
-        assertFalse(game.makeMove(0, 0, 'S'));
-        assertEquals(Cell.S, game.getCell(0, 0));
-    }
-
-    @Test
-    public void testInvalidMoveOutsideBoard() {
-        assertFalse(game.makeMove(3, 3, 'S'));
-    }
-
-    @Test
-    public void testInvalidMoveOnFilledCell() {
-        game.makeMove(1, 1, 'S');
-        assertFalse(game.makeMove(1, 1, 'O'));
-    }
-
-   
-
-    @Test
-    public void testGameDoesNotEndOnSOSFormation() {
-        game.makeMove(0, 0, 'S');
-        game.makeMove(0, 1, 'O');
-        game.makeMove(0, 2, 'S');
-        assertFalse(game.isGameOver());
-    }
-
-    @Test
-    public void testGameEndsWhenBoardIsFull() {
-        game.makeMove(0, 0, 'S');
-        game.makeMove(0, 1, 'O');
-        game.makeMove(0, 2, 'S');
-        game.makeMove(1, 0, 'S');
-        game.makeMove(1, 1, 'O');
-        game.makeMove(1, 2, 'S');
-        game.makeMove(2, 0, 'S');
-        game.makeMove(2, 1, 'O');
-        game.makeMove(2, 2, 'S');
-        assertTrue(game.isGameOver());
+        assertTrue(game.isGameOver()); // The board is full, so the game should be over.
     }
 }
